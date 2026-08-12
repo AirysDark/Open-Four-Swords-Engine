2f5c2df41f29936c92052a74cbaadec5c0e69fcf
AirysDark <47623717+AirysDark@users.noreply.github.com>
2026-08-13 05:49:03 +1000

    Begin Zelda3 core integration

---
+++ engine/adapters/Zelda3WorldAdapter.cpp
@@ -0,0 +1,18 @@
+#include "Zelda3WorldAdapter.h"
+
+namespace Engine {
+
+void Zelda3WorldAdapter::initialize()
+{
+}
+
+void Zelda3WorldAdapter::loadRoom(int roomId)
+{
+    (void)roomId;
+}
+
+void Zelda3WorldAdapter::update()
+{
+}
+
+}
