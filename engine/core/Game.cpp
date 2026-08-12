1ad0ae3b69fc5e329dc20abdf392da2e46eac042
AirysDark <47623717+AirysDark@users.noreply.github.com>
2026-08-13 05:46:26 +1000

    Initial engine skeleton

---
+++ engine/core/Game.cpp
@@ -0,0 +1,17 @@
+#include "Game.h"
+
+namespace Engine {
+
+Game::Game() : running(false) {}
+
+void Game::run()
+{
+    running = true;
+    while (running)
+    {
+        // Main engine loop placeholder
+        running = false;
+    }
+}
+
+}
