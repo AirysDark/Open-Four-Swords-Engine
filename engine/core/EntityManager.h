3eb5f3f8d1b8d29b04bf61f64c632995674969c0
AirysDark <47623717+AirysDark@users.noreply.github.com>
2026-08-13 05:55:50 +1000

    Add ECS foundation update

--- engine/core/EntityManager.h
+++ engine/core/EntityManager.h
@@ -8,10 +8,13 @@ namespace OFSE {
 class EntityManager
 {
 public:
+    Entity* create();
+    void destroy(Entity* entity);
     void update(float deltaTime);
     void add(Entity* entity);
 
 private:
+    int nextId = 1;
     std::vector<Entity*> entities;
 };
 
